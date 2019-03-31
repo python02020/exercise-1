985a575f237083bfa59208b708964ad4d7e5a407
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-03-31 17:18:01 +0200

    added test

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
-import code
+import ex1
 import unittest
 import random
 
 class TestStringMethods(unittest.TestCase):
 
     def test(self):
-        self.assertEqual(type(code.myFunction(0)), str)
+        self.assertEqual(type(ex1.makeString(0)), str)
         for i in range(5):
             num = random.randint(1,101)
-        self.assertEqual(len(code.myFunction(num)),  num)
+            self.assertEqual(len(ex1.makeString(num)),  num)
+        self.assertRaises(Exception, ex1.makeString, -1)
 
 if __name__ == '__main__':
     unittest.main()
