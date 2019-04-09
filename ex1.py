a150abe7534d71d1563e408fe69fcd1b51b9d64d
Bao Minh Doan Dang <doandang.baominh@yahoo.com>
2019-04-09 22:48:28 +0200

    firsttry

--- ex1.py
+++ ex1.py
@@ -1,3 +1,9 @@
-def makeString(x):
-    return None
+def makeString(num):
+    stringChr = "a"
+    if num < 0:   #RAISING EXCEPTION IF X IS BELOW 0
+        raise Exception("Number not allowed")
+    else:
+        stringChr = "a"*num
+    
+    return stringChr
     
