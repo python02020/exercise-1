b138bda662bb5baccdfce56680da4f7b1cb254b4
Dominik Gerstorfer <dominik.gerstorfer@gmail.com>
2019-04-10 09:17:59 +0200

    Ok

--- ex1.py
+++ ex1.py
@@ -1,3 +1,8 @@
 def makeString(x):
-    return None
-    
+    returnStr = ''
+    if x < 0:
+        raise
+    else:
+        for i in range(x):
+            returnStr += ' '
+        return returnStr 
