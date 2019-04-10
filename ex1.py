edaa323a7e5b0d71e448da324d6bc54ae34d11c5
Vanessa Fscher <fischer-vanessa@web.de>
2019-04-10 21:42:31 +0200

    init

--- ex1.py
+++ ex1.py
@@ -1,3 +1,13 @@
-def makeString(x):
-    return None
-    
+def makeString(intInput):
+    s = ""
+    try:
+        if intInput > 0:
+            result = "a" * intInput
+            return result
+        elif intInput == 0:
+            return s
+        elif intInput < 0:
+            print("Error! No negative integers allowed.")
+            return None
+    except:
+        print("Input is not an integer")
