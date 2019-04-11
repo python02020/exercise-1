596a8a3e9675992900a1e7df28ae26902c8c6336
Thomas Wacker <wacker.thomas@live.de>
2019-04-11 18:23:00 +0200

    Sinnvolles Kommentar?

--- ex1.py
+++ ex1.py
@@ -1,3 +1,10 @@
 def makeString(x):
-    return None
+    counter = int(x)
+    string = ""
+    if counter < 0 :
+        raise Exception("You cannot enter a value below zero.")
+    else:
+        string = counter * "a"
+        return string
+
     
