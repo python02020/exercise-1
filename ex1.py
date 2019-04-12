e1eab8c0415c109b03fc9828de48559c18bb3f5a
Vanessa Fscher <fischer-vanessa@web.de>
2019-04-12 18:43:12 +0200

    ChangeException

--- ex1.py
+++ ex1.py
@@ -1,13 +1,6 @@
 def makeString(intInput):
-    s = ""
-    try:
-        if intInput > 0:
-            result = "a" * intInput
-            return result
-        elif intInput == 0:
-            return s
-        elif intInput < 0:
-            print("Error! No negative integers allowed.")
-            return None
-    except:
-        print("Input is not an integer")
+    if intInput < 0:
+        raise Exception('No negative integers allowed.')
+    else:
+        resultString = 'a' * intInput
+    return resultString
